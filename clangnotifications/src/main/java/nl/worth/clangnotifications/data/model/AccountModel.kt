4dad8c5daf7c3340c58a728e58f8b6c2c17d800d
tpadalko <tpadalko@worth.systems>
2020-01-09 14:03:36 +0100

    changed field name

--- clangnotifications/src/main/java/nl/worth/clangnotifications/data/model/AccountModel.kt
+++ clangnotifications/src/main/java/nl/worth/clangnotifications/data/model/AccountModel.kt
@@ -1,3 +1,3 @@
 package nl.worth.clangnotifications.data.model
 
-data class AccountModel(val id: String, val tokens: Array<String>)
+data class AccountModel(val userId: String, val tokens: Array<String>)
