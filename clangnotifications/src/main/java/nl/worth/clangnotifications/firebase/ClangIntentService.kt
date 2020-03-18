218d2a627b5a54191d59354ceea749abd2aa45c2
Otto Boekesteijn <oboekesteijn@worth.systems>
2020-03-18 18:36:32 +0100

    EVNS-29: also use secure shared preferences for getting userId for sending notification action

--- clangnotifications/src/main/java/nl/worth/clangnotifications/firebase/ClangIntentService.kt
+++ clangnotifications/src/main/java/nl/worth/clangnotifications/firebase/ClangIntentService.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.widget.Toast
 import androidx.core.app.NotificationManagerCompat
 import nl.worth.clangnotifications.data.interactor.NotificationInteractor
+import nl.worth.clangnotifications.util.getUserId
 
 internal class ClangIntentService : IntentService("ClangIntentService") {
     override fun onHandleIntent(intent: Intent?) {
@@ -12,7 +13,7 @@ internal class ClangIntentService : IntentService("ClangIntentService") {
         val notificationId = intent?.getStringExtra("notificationId")
 
         if (notificationId != null && id != null) {
-            NotificationInteractor().logNotificationAction(notificationId, "userId", id, "secret",
+            NotificationInteractor().logNotificationAction("authHeader", notificationId, applicationContext.getUserId(), id,
                 {
                     Toast.makeText(applicationContext, "Thank you for your submit!", Toast.LENGTH_LONG).show()
                 },
