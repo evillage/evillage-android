877ddf376493832ebe85b7340457c2a7685df9b7
a.boutakidis <a.boutakidis@sping.nl>
2020-07-01 14:54:32 +0200

    - resolved MasterKeys and EncryptedSharedPreferences deprecations - added EncryptedSharedPreferences file name and variable key to constants

--- clangnotifications/src/main/java/nl/worth/clangnotifications/util/Extensions.kt
+++ clangnotifications/src/main/java/nl/worth/clangnotifications/util/Extensions.kt
@@ -3,7 +3,8 @@ package nl.worth.clangnotifications.util
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
+import androidx.security.crypto.MasterKey
+
 
 /**
  * Saves the registered user id into [EncryptedSharedPreferences]
@@ -11,35 +12,33 @@ import androidx.security.crypto.MasterKeys
  * @param userId The id of the current user's registered account
  */
 internal fun Context.saveUserId(userId: String) {
-    val masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
-    val sharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
-        "clang",
-        masterKeyAlias,
-        this,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
-
-    sharedPreferences.edit().apply {
-        putString("user.id", userId)
-        apply()
-    }
+    getSharedPreferences(this).edit().putString(userIdKey, userId).apply()
 }
 
 /**
  * Fetches the user id from [EncryptedSharedPreferences]
  */
 internal fun Context.getUserId(): String? {
-    val masterKeyAlias: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    return getSharedPreferences(this).getString(userIdKey, null)
+}
 
-    val sharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
-        "clang",
-        masterKeyAlias,
-        this,
+/**
+ * Creates and returns an EncryptedSharedPreferences instance
+ *
+ * @return SharedPreferences instance
+ */
+fun getSharedPreferences(context: Context): SharedPreferences {
+    val masterKey = MasterKey
+        .Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    return EncryptedSharedPreferences.create(
+        context,
+        clangFile,
+        masterKey,
         EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
         EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
     )
-
-    return sharedPreferences.getString("user.id", null)
 }
 
