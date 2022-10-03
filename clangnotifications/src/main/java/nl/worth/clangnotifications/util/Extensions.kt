1122c07e6629090f7fe56ca5c7d2f6b78208100c
Livecast71 <35631641+Livecast71@users.noreply.github.com>
2022-10-03 15:20:39 +0200

    Update Extensions.kt

--- clangnotifications/src/main/java/nl/worth/clangnotifications/util/Extensions.kt
+++ clangnotifications/src/main/java/nl/worth/clangnotifications/util/Extensions.kt
@@ -44,54 +44,25 @@ fun makeSharedPreferences(context: Context): SharedPreferences {
     context.getSharedPreferences(clangFile, Context.MODE_PRIVATE).edit().clear().apply()
 
 
-    val mainKey = MasterKey.Builder(context)
+    val masterKey = MasterKey.Builder(context)
         .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
         .build()
 
 
-    if (mainKey.isKeyStoreBacked) {
-
-
-        val masterKey = MasterKey
-            .Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
-            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-            .setUserAuthenticationRequired(false)
-            .build()
-
-        return EncryptedSharedPreferences.create(
-            context,
-            clangFile,
-            masterKey,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
-
-    } else {
-
-        val masterKey = MasterKey
-            .Builder(context, "ClangCode")
-            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-            .setUserAuthenticationRequired(false)
-            .build()
-
-        return EncryptedSharedPreferences.create(
-            context,
-            clangFile,
-            masterKey,
-            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
-
-    }
+    return EncryptedSharedPreferences.create(
+        context,
+        clangFile,
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
 
 }
 
 fun getSharedPreferences(context: Context): SharedPreferences {
 
-    val masterKey = MasterKey
-        .Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+    val masterKey = MasterKey.Builder(context)
         .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-        .setUserAuthenticationRequired(false)
         .build()
-
 
 
 
