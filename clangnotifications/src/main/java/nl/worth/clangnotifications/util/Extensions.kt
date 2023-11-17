ae1a3a418c434ea9c06d5ad04d4ae512e6dd077a
Livecast71 <35631641+Livecast71@users.noreply.github.com>
2023-11-17 10:22:23 +0100

    Adjustments to ClangImplementation and Extensions

--- clangnotifications/src/main/java/nl/worth/clangnotifications/util/Extensions.kt
+++ clangnotifications/src/main/java/nl/worth/clangnotifications/util/Extensions.kt
@@ -3,81 +3,23 @@ package nl.worth.clangnotifications.util
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.annotation.Keep
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKey
-import androidx.security.crypto.MasterKeys
-import java.security.KeyStore
 
 
-/**
- * Saves the registered user id into [EncryptedSharedPreferences]
- *
- * @param userId The id of the current user's registered account
- */
 @Keep
 fun Context.saveUserId(userId: String) {
-    makeSharedPreferences(this).edit().putString(userIdKey, userId).apply()
+    val sharedPreferences: SharedPreferences =
+        getSharedPreferences("clang_preferences", Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+    editor.putString("userId", userId)
+    editor.apply()
 }
 
-/**
- * Fetches the user id from [EncryptedSharedPreferences]
- */
 @Keep
 fun Context.getUserId(): String? {
-    return getSharedPreferences(this).getString(userIdKey, null)
+    val sharedPreferences: SharedPreferences =
+        getSharedPreferences("clang_preferences", Context.MODE_PRIVATE)
+    return sharedPreferences.getString("userId", null)
 }
-
-/**
- * Creates and returns an EncryptedSharedPreferences instance
- *
- * @return SharedPreferences instance
- */
-@Keep
-
-
-fun makeSharedPreferences(context: Context): SharedPreferences {
-
-    val keyStore: KeyStore = KeyStore.getInstance("AndroidKeyStore")
-    keyStore.load( /* param= */null)
-    keyStore.deleteEntry(MasterKey.DEFAULT_MASTER_KEY_ALIAS);
-
-    context.getSharedPreferences(clangFile, Context.MODE_PRIVATE).edit().clear().apply()
-
-
-    val masterKey = MasterKey.Builder(context)
-        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-        .build()
-
-
-    return EncryptedSharedPreferences.create(
-        context,
-        clangFile,
-        masterKey,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
-
-}
-
-fun getSharedPreferences(context: Context): SharedPreferences {
-
-    val masterKey = MasterKey.Builder(context)
-        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
-        .build()
-
-
-
-    return EncryptedSharedPreferences.create(
-        context,
-        clangFile,
-        masterKey,
-        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-    )
-}
-
-
-
-
 
 
 
